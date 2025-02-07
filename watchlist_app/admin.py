4ec315d07e4afa9a79e204db74c51981c3b8f209
cyber1709 <reachgauravarora@outlook.com>
2025-02-07 16:21:44 +0530

    nested serializer and relational model added

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from watchlist_app.models import WatchList, StreamPlatform
 
 
+class WatchListAdmin(admin.ModelAdmin):
+    list_display = ('title', 'storyline', 'active', 'platform', 'created',)
+    search_fields = ['title']
+    list_filter = ['active', 'created']
+
+admin.site.register(WatchList, WatchListAdmin)
 
 
-admin.site.register(WatchList)
-admin.site.register(StreamPlatform)
+class StreamPlatformAdmin(admin.ModelAdmin):
+    list_display = ('name', 'about', 'website')
+
+admin.site.register(StreamPlatform, StreamPlatformAdmin)
 
 # Register your models here.
