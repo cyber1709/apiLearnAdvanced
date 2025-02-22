64d6d27f99b979bae320e6fe5f69a2ee88eb5607
cyber1709 <reachgauravarora@outlook.com>
2025-02-22 13:41:17 +0530

    token generated on registration and returned on login

--- user_app/views.py
+++ user_app/views.py
@@ -2,25 +2,25 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from .serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
 
 @api_view(['POST'])
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+        data = {}
         if serializer.is_valid():
-            user = serializer.save()  # Save the user
-            return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
-from .serializers import RegistrationSerializer
-
-@api_view(['POST'])
-def registration_view(request):
-    if request.method == 'POST':
-        serializer = RegistrationSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()  # Save the user
-            return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            account = serializer.save()
+            data['response'] = 'Registration Successfull!'
+            data['username'] = account.username
+            data['email'] = account.email
+            
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+            
+        else:
+           data = serializer.errors
+            
+            
+            
+        return Response(data)
