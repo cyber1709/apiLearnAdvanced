ea3cc83155a35e28ea9a43849b8c1ae412b864e9
cyber1709 <reachgauravarora@outlook.com>
2025-02-22 13:52:02 +0530

    logout added successfull, everything working fine!

--- user_app/views.py
+++ user_app/views.py
@@ -4,6 +4,15 @@ from rest_framework import status
 from .serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
 
+
+
+@api_view(['POST'])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+    
+    
 @api_view(['POST'])
 def registration_view(request):
     if request.method == 'POST':
